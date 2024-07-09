5c569f7ec96760188e0491d9b514f7793465816f
Vikas Singh <vikas.vm.singh@gmail.com>
2024-07-09 09:47:21 +0530

    initial

---
+++ store/app/(routes)/page.tsx
@@ -0,0 +1,24 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const billboard = await getBillboard('0ae90841-8819-4984-a906-ba5975d4d0c9');
+    const products = await getProducts({ isFeatured: true })
+    return (
+        <Container>
+            <div className="pb-10 space-y-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage;
